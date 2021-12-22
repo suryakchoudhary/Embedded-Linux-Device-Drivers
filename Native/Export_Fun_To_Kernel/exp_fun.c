d4e48c175d5e58bb19cc497d9689e604b8a111a2
suryakchoudhary <surya333kant@gmail.com>
2021-12-22 17:43:40 +0530

    Function Call Module Programming

---
+++ Native/Export_Fun_To_Kernel/exp_fun.c
@@ -0,0 +1,26 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("SURYA");
+
+static int Add(int a, int b)
+{
+        return (a+b);
+}
+
+EXPORT_SYMBOL_GPL(Add);
+
+static int fun_init(void)
+{
+        printk(KERN_ALERT "\nExporting Symbol\n");
+        return 0;
+}
+
+static void fun_exit(void)
+{
+        printk(KERN_ALERT "\nSymbol Cleanup\n");
+}
+
+module_init(fun_init);
+module_exit(fun_exit);
